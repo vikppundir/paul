c9a0af684496d277d78532b2cfdc643f4b5a034f
vikppundir <59640628+vikppundir@users.noreply.github.com>
2023-02-20 15:52:10 +0530

    category

--- view/dashboad/admin/category.php
+++ view/dashboad/admin/category.php
@@ -5,7 +5,7 @@
     <?php  $categorysql = sql("select * from category where id !='{$categoryId}' and parentId IN('',0) and is_active = 1")->data; ?>
     <?php  $categoryMenu = sql("SELECT c1.* FROM  {$this->table} c1 LEFT JOIN category c2 on c2.id = c1.parentId where c1.type ='{$this->type}' ORDER BY COALESCE(c2.id, c1.id), c1.id and c1.is_active = 1 limit 10")->data; ?>
     <?php  $categorySignle = sql("select * from category where id='{$categoryId}' and name = '{$categoryName}' and is_active = 1")->data[0]??null; ?>
-    <?php  $categoryCount = sql("select count(*) as count from category where c1.type='{$type}' and  is_active = 1") ?>
+    <?php  $categoryCount = sql("select count(*) as count from category where type='{$type}' and  is_active = 1") ?>
    
     
 <div class="main-content d-flex">
